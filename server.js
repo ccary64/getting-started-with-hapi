c6c801e9c3d406cfaf500336e4bae7543244ce4b
ChrisC <ccary64@gmail.com>
2017-12-27 20:20:27 -0500

    take out route

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const FrontEnd = require('./frontEnd');
 const server = Hapi.server({ host: '0.0.0.0', port: 3000 });
 
 const api = new Api(server);
-
 const frontEnd = new FrontEnd(server);
-frontEnd.routes();
 
 server.start();
